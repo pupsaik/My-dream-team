62bba5d09dfdb3fc1f4b7e4afef4fc45984b5b45
pupsaik <151430709+pupsaik@users.noreply.github.com>
2024-11-18 21:30:03 +0200

    Add Yurii's info

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,10 +4,10 @@ const Body = () => {
   return (
     <main style={styles.body}>
       <div style={styles.card}>
-        <img src={"/avatar-default.png"} alt="John Doe" style={styles.photo} />
+        <img src={"/stelmakh_yurii.png"} alt="StelmakhYurii" style={styles.photo} />
         <div style={styles.textContainer}>
-          <h2 style={styles.name}>John Doe</h2>
-          <p style={styles.position}>Software engineer</p>
+          <h2 style={styles.name}>Stelmakh Yurii</h2>
+          <p style={styles.position}>Scrum Master</p>
         </div>
       </div>
     </main>
